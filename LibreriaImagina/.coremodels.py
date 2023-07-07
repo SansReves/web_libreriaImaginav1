8ebf1aa68972027289a609ff3447b2385f938ce9
SansReves <ig.mendezs@duocuc.cl>
2023-07-06 23:55:26 -0400

    connection database

--- LibreriaImagina/.coremodels.py
+++ LibreriaImagina/.coremodels.py
@@ -7,8 +7,78 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128, blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.CharField(max_length=254, blank=True, null=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class CatgLibro(models.Model):
-    id_catg = models.AutoField(primary_key=True)
+    id_catg = models.IntegerField(primary_key=True)
     catg_libro = models.CharField(max_length=30)
 
     class Meta:
@@ -32,13 +102,59 @@ class Cliente(models.Model):
 
 
 class Despacho(models.Model):
-    id_despc = models.AutoField(primary_key=True)
+    id_despc = models.IntegerField(primary_key=True)
     nombre_despc = models.CharField(max_length=30)
     precio_despc = models.IntegerField()
 
     class Meta:
         managed = False
         db_table = 'despacho'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200, blank=True, null=True)
+    action_flag = models.IntegerField()
+    change_message = models.TextField(blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100, blank=True, null=True)
+    model = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField(blank=True, null=True)
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
 
 class Empleado(models.Model):
     rut_emp = models.CharField(primary_key=True, max_length=9)
@@ -56,7 +172,7 @@ class Empleado(models.Model):
 
 
 class Estado(models.Model):
-    id_est = models.AutoField(primary_key=True)
+    id_est = models.IntegerField(primary_key=True)
     nombre_estado = models.CharField(max_length=40)
 
     class Meta:
@@ -65,7 +181,7 @@ class Estado(models.Model):
 
 
 class Libro(models.Model):
-    id_libro = models.AutoField(primary_key=True)
+    id_libro = models.IntegerField(primary_key=True)
     nombre_libro = models.CharField(max_length=40)
     autor_libro = models.CharField(max_length=40)
     sinopsis_libro = models.CharField(max_length=250, blank=True, null=True)
@@ -80,7 +196,7 @@ class Libro(models.Model):
 
 
 class OrdenCompra(models.Model):
-    id_compra = models.BigAutoField(primary_key=True)
+    id_compra = models.IntegerField(primary_key=True)
     fecha_or_compra = models.DateField()
     total_final_or = models.IntegerField(blank=True, null=True)
     rut_emp = models.ForeignKey(Empleado, models.DO_NOTHING, db_column='rut_emp')
@@ -133,7 +249,7 @@ class OrdenServicio(models.Model):
 
 
 class Servicio(models.Model):
-    id_serv = models.AutoField(primary_key=True)
+    id_serv = models.IntegerField(primary_key=True)
     nombre_serv = models.CharField(max_length=50)
     dscrp_serv = models.CharField(max_length=250, blank=True, null=True)
     precio_serv = models.IntegerField()
@@ -144,7 +260,7 @@ class Servicio(models.Model):
 
 
 class TipoCliente(models.Model):
-    id_tp_cli = models.AutoField(primary_key=True)
+    id_tp_cli = models.IntegerField(primary_key=True)
     nombre_tp_cli = models.CharField(max_length=20)
 
     class Meta:
@@ -153,7 +269,7 @@ class TipoCliente(models.Model):
 
 
 class TipoEmpleado(models.Model):
-    id_tp_emp = models.AutoField(primary_key=True)
+    id_tp_emp = models.IntegerField(primary_key=True)
     nombre_tp_emp = models.CharField(max_length=30)
 
     class Meta:
@@ -162,7 +278,7 @@ class TipoEmpleado(models.Model):
 
 
 class TipoPago(models.Model):
-    id_tp_pago = models.AutoField(primary_key=True)
+    id_tp_pago = models.IntegerField(primary_key=True)
     nombre_tp_pago = models.CharField(max_length=20)
 
     class Meta:
--- LibreriaImagina/LibreriaImagina/settings.py
+++ LibreriaImagina/LibreriaImagina/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'LibreriaImagina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'bdLibreria',
+        'PASSWORD': 'bdLibreria',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
     }
 }
 
