5bb4b6c036d659a30d2c51a936c69e28a48e55dc
SansReves <ig.mendezs@duocuc.cl>
2023-07-07 19:50:34 -0400

    create base and home

---
+++ LibreriaImagina/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('catalogo/', catalogo, name="catalogo"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+
+]
