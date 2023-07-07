d9595e5bc01dc3ae4dd9b0c584fd5800e0bc4502
SansReves <ig.mendezs@duocuc.cl>
2023-07-07 19:51:09 -0400

    Changes to be committed: 	modified:   LibreriaImagina/LibreriaImagina/__pycache__/urls.cpython-311.pyc 	modified:   LibreriaImagina/LibreriaImagina/urls.py 	new file:   LibreriaImagina/core/__pycache__/urls.cpython-311.pyc 	new file:   LibreriaImagina/core/__pycache__/views.cpython-311.pyc 	new file:   LibreriaImagina/core/static/core/CSS/base.css 	new file:   LibreriaImagina/core/static/core/CSS/bootstrap.css 	new file:   LibreriaImagina/core/static/core/CSS/home.css 	new file:   LibreriaImagina/core/static/core/IMG/Cart_512.png 	new file:   LibreriaImagina/core/static/core/IMG/book_bug.png 	new file:   LibreriaImagina/core/static/core/IMG/book_smile.png 	new file:   LibreriaImagina/core/static/core/IMG/job_book_honya.png 	new file:   LibreriaImagina/core/static/core/IMG/study_group.png 	new file:   LibreriaImagina/core/static/core/IMG/usuario.png 	new file:   LibreriaImagina/core/static/core/JS/additional-methods.js 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.bundle.js 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.bundle.js.map 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.bundle.min.js 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.bundle.min.js.map 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.js 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.js.map 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.min.js 	new file:   LibreriaImagina/core/static/core/JS/bootstrap.min.js.map 	new file:   LibreriaImagina/core/static/core/JS/jquery-3.3.1.slim.min.js 	new file:   LibreriaImagina/core/static/core/JS/jquery.validate.js 	new file:   LibreriaImagina/core/static/core/JS/jquery.validate.min.js 	new file:   LibreriaImagina/core/static/core/JS/popper.min.js 	new file:   LibreriaImagina/core/templates/core/base.html 	new file:   LibreriaImagina/core/templates/core/catalogo.html 	new file:   LibreriaImagina/core/templates/core/home.html 	new file:   LibreriaImagina/core/templates/core/iniciosesion.html 	modified:   LibreriaImagina/core/urls.py 	modified:   LibreriaImagina/core/views.py

--- LibreriaImagina/core/urls.py
+++ LibreriaImagina/core/urls.py
@@ -4,11 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('iniciosesion/', iniciosesion, name="iniciosesion"),
-    path('catalogo/', catalogo, name="catalogo"),
-    path('iniciosesion/', iniciosesion, name="iniciosesion"),
-    path('iniciosesion/', iniciosesion, name="iniciosesion"),
-    path('iniciosesion/', iniciosesion, name="iniciosesion"),
-    path('iniciosesion/', iniciosesion, name="iniciosesion"),
-    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+
+    
 
 ]
